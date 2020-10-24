c37a9fb5043018e9a52f0c383f4ca70dae9fc985
Alfian Naufal <piant.grunger@gmail.com>
2020-10-24 22:05:33 +0700

    Update topnav.php

--- views/layouts/topnav.php
+++ views/layouts/topnav.php
@@ -15,7 +15,7 @@
   </div>
 </form>
 <ul class="navbar-nav navbar-right">
-  <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg{{ Auth::user()->unreadNotifications->count() ? ' beep' : '' }}"><i class="far fa-bell"></i></a>
+  <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg"><i class="far fa-bell"></i></a>
     <div class="dropdown-menu dropdown-list dropdown-menu-right">
       <div class="dropdown-header">Notifications
         <div class="float-right">
